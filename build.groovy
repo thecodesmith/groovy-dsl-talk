7d10ec95e7235c8c82413165971f8a284097e7e4
Brian Stewart <brian@thecodesmith.com>
2015-04-19 22:33:17 -0500

    Created build script to generate index.html file based on slides in "md/" dir

---
+++ build.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+markdownDir = 'md'
+markdownFilter = { dir, file -> file ==~ /.*\.md$/ } as FilenameFilter
+markdownFiles = new File(markdownDir).list markdownFilter
+
+engine = new groovy.text.SimpleTemplateEngine()
+slide = ' ' * 20 + '<section data-markdown="$dir/$file"></section>'
+
+slideTags = []
+
+markdownFiles.each { file ->
+    def template = engine.createTemplate(slide).make([dir: markdownDir, file: file])
+    slideTags << template.toString()
+}
+
+tags = slideTags.join('\n')
+
+templateFile = 'index-template.html'
+outputFile = 'index.html'
+templateText = new File(templateFile).text
+
+new File(outputFile).withWriter { out ->
+    out.write templateText.replace('REPLACE_WITH_SLIDE_TAGS', tags)
+}
